971eaea0b528ce6c68ac6f19d881052936f255ac
zokresz <00001110232160sp@al.educacao.sp.gov.br>
2024-09-13 11:02:14 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//variáveis da bolinha
+let xBolinha = 450;
+let yBolinha = 20;
+let diametro = 19;
+let raio = diametro / 2;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 8;
+let velocidadeYBolinha = 8;
+
+//variáveis da raquete
+let xRaquete = 6;
+let yRaquete = 155;
+let raqueteComprimento = 10;
+let raqueteAltura = 50;
+
+//variáveis do oponente
+let xRaqueteOponente = 583;
+let yRaqueteOponente = 100;
+let velocidadeYOponente;
+
+//placar do jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
+
+//sons do jogo
+let raquetada;
+let ponto;
